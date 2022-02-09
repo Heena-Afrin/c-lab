8e69d644af829b5aa8237018402b713234e43c9e
Heena-Afrin <97308616+Heena-Afrin@users.noreply.github.com>
2022-02-09 23:02:44 +0530

    addition and subtraction using overloading
    addition and subtraction using operator overloading

---
+++ experiment16/main (20).cpp
@@ -0,0 +1,56 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+class Complex{
+public:
+ float real;
+ float img;
+ Complex(){
+ this->real = 0.0;
+ this->img = 0.0;
+ }
+ Complex(float real, float img){
+ this->real = real;
+ this->img = img;
+ }
+ Complex operator+(const Complex &obj){
+ Complex temp;
+ temp.img = this->img + obj.img;
+ temp.real = this->real + obj.real;
+ return temp;
+ }
+ Complex operator-(const Complex &obj){
+ Complex temp;
+ temp.img = this->img - obj.img;
+ temp.real = this->real - obj.real;
+ return temp;
+ }
+ void display(){
+ cout << this->real << " + " << this->img << "i" << endl;
+ }
+};
+int main()
+{
+ Complex a, b, c;
+ cout << "Enter real and complex coefficient of the first complex number: " << endl;
+ cin >> a.real;
+ cin >> a.img;
+ cout << "Enter real and complex coefficient of the second complex number: " << endl;
+ cin >> b.real;
+ cin >> b.img;
+ cout << "Addition Result: ";
+ c = a+b;
+ c.display();
+ cout << "Subtraction Result: ";
+ c = a-b;
+ c.display();
+ return 0;
+    
+}
